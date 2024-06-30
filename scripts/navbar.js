cae51fd7bfe09e1c72ae118d7e6df55d4bd2ac43
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2024-06-30 23:06:33 +0200

    Add navbar

---
+++ scripts/navbar.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('afterbegin', data);
+
+            let lastScrollTop = 0;
+            const navbar = document.querySelector('.navbar');
+
+            window.addEventListener('scroll', function() {
+                let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                if (scrollTop > lastScrollTop) {
+                    // Scrolling down
+                    navbar.classList.add('navbar-hidden');
+                } else {
+                    // Scrolling up
+                    navbar.classList.remove('navbar-hidden');
+                }
+                lastScrollTop = scrollTop;
+            });
+        });
+});
